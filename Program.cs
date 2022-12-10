c9d15401aa8543f596d6cb4820b4a2162d164377
kasat <kasat@DESKTOP-DOQKB49>
2022-12-10 16:03:30 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// порушено принцип ISP
+
+using System.Security.Cryptography.X509Certificates;
+
+interface IDiscount { void ApplyDiscount(String discount); }
+interface IPromocode {void ApplyPromocode(String promocode);}
+interface IColor {void SetColor(byte color);}
+interface ISize {void SetSize(byte size);}
+interface IPrice { void SetPrice(double price); }
+class Overdress : IPrice, IDiscount, IPromocode, IColor, ISize
+{
+    public void ApplyPromocode(String promocode) { }
+    public void SetPrice(double price) { }
+    public void ApplyDiscount(String discount) { }
+    public void SetColor(byte color) { }
+    public void SetSize(byte size) { }
+
+}
+    class Book : IPrice , IDiscount, IPromocode 
+{
+    public void ApplyPromocode(String promocode) { }
+    public void SetPrice(double price) { }
+    public void ApplyDiscount(String discount) { }
+}
